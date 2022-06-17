972d0bc3a853968184784f800c5ba33bea111281
Shahzad <mech1090@gmail.com>
2022-06-17 07:09:24 -0600

    service created

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -2,6 +2,7 @@ const bcrypt = require('bcrypt')
 const config = require('config')
 const {validationSchema} = require('../validation/user.validation')
 const User = require('../model/user.model')
+const serviceUser = require('../service/user.service')
 
 
 const getLoginFrom = (req,res)=>{
@@ -11,11 +12,12 @@ const getLoginFrom = (req,res)=>{
 const login = async(req,res)=>{
     const {email,password} = req.body
     const fields = {email,password}
-    const findUser = await User.findOne({email})
+    const findUser = await serviceUser.findEmail({email})
+ //   const findUser = await User.findOne({email})
     if(!findUser){
         return res.render('signup/layout',{message:'User does not exist Signup'})
     }
-    const matchPassword = await bcrypt.compare(password,findUser.password)
+   const matchPassword = await bcrypt.compare(password,findUser.password)
     if(!matchPassword){
         return res.render('login/layout',{message:'Credentials Mismatched'})
     }
@@ -34,11 +36,11 @@ const signup = async(req,res)=>{
         return res.render('signup/layout',{message:error.details[0].message})
     }
     const hashPassword = await bcrypt.hash(password,config.get('hash.salt'))
-    const findUser = await User.findOne({email})
+    const findUser = await serviceUser.findEmail({email})
     if(findUser){
         return res.render('login/layout',{message:'User already exist'})
     }
-    const createUser = await User.create({email,password:hashPassword})
+    const createUser = await serviceUser.CreateFields({email,password:hashPassword})
     return res.render('signup/layout',{message:`${createUser.email} is created`})
 
 }
